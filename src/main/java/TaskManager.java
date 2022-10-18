76da76d5f2b6d86fe159272499b7472064df4086
karolina-wisniewska <karolina.raczkowska.94@gmail.com>
2022-10-18 19:52:47 +0200

    add

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -5,10 +5,13 @@ import java.util.Scanner;
 
 public class TaskManager {
 
+    static String[][] tasks;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        tasks = readFromFile();
         String option = "";
+
         while(!option.equals("exit")) {
             printMenu();
             option = scanner.nextLine();
@@ -30,27 +33,62 @@ public class TaskManager {
                     break;
             }
         }
+        scanner.close();
+    }
+
+    public static String[][] readFromFile(){
+        tasks = new String[0][];
+        File file = new File("tasks.csv");
+
+        try(Scanner sc = new Scanner(file)){
+            while (sc.hasNextLine()){
+                String line = sc.nextLine();
+                String[] data = line.split(",");
+                tasks = addNewRow(tasks, data);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return tasks;
     }
     public static void printMenu() {
         System.out.println(ConsoleColors.GREEN_BOLD + "\nPlease select an option:");
         System.out.println(ConsoleColors.RESET + "add\n" +
                 "remove\n" +
                 "list\n" +
-                "exit");
+                "exit\n");
     }
 
-    public static void add(){
-        System.out.println("Option add executing...");
+
+
+    public static String[][] add(){
+        Scanner scanner2 = new Scanner(System.in);
+        String[] newTask = new String[3];
+        System.out.println("Please add task description");
+        newTask[0] = scanner2.nextLine();
+        System.out.println("Please add task due date");
+        newTask[1] = scanner2.nextLine();
+        System.out.println("Is your task important: " + ConsoleColors.RED + "true" + ConsoleColors.RESET + "/" + ConsoleColors.RED + "false");
+        while (!scanner2.hasNextBoolean()) {
+            System.out.println(ConsoleColors.RED_BOLD + "Invalid value. Please enter boolean: " + ConsoleColors.RED + "true" + ConsoleColors.RESET + "/" + ConsoleColors.RED + "false" );
+            scanner2.nextLine();
+        }
+        newTask[2] = scanner2.nextLine();
+        scanner2.close();
+        System.out.println(ConsoleColors.RESET);
+        tasks = addNewRow(tasks,newTask);
+        return tasks;
     }
 
     public static void remove(){
+
         System.out.println("Option remove executing...");
     }
 
     public static void list(){
         System.out.println("list");
-        String[][] tasks = readFromFile();
-        for(int i=0; i<tasks.length; i++){
+        for(int i=0; i < tasks.length; i++){
             System.out.println(i + " : " + tasks[i][0] + "\t" +
                     tasks[i][1] + "\t" + ConsoleColors.RED +
                     tasks[i][2] + ConsoleColors.RESET);
@@ -63,22 +101,7 @@ public class TaskManager {
 
     }
 
-    public static String[][] readFromFile(){
-        String[][] uploadedTasks = new String[0][];
-        File file = new File("tasks.csv");
 
-        try(Scanner sc = new Scanner(file)){
-            while (sc.hasNextLine()){
-                String line = sc.nextLine();
-                String[] data = line.split(",");
-                uploadedTasks = addNewRow(uploadedTasks, data);
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        return uploadedTasks;
-    }
 
     public static String[][] addNewRow(String[][] multiarray, String[] row){
         multiarray = Arrays.copyOf(multiarray, multiarray.length + 1);
